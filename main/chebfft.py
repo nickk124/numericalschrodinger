ed05036917449a02502a351aa1a64c5f4a424006
nickk124 <37184532+nickk124@users.noreply.github.com>
2018-11-17 13:44:29 -0500

    test

--- main/chebfft.py
+++ main/chebfft.py
@@ -2,9 +2,3 @@ import numpy as np
 import numpy.polynomial.chebyshev as cheb
 import schrodingerutils
 
-
-#initializes an array for wavefunction values, where X is the number of x positions on the grid,
-# and T is the number of time steps
-X = 10000
-T = 1000
-psi = np.array((T,X))
