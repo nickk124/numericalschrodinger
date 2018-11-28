6b5af06d38fe0edd4ad963a96b55d619d11acfab
tmarshall1218 <rosstom@live.unc.edu>
2018-11-27 23:45:55 -0500

    fixed indexing to make CN work

--- main/CrankDat.py
+++ main/CrankDat.py
@@ -54,9 +54,8 @@ def cranknicholson(x,t,potential,delt,delx,fBNC,psi_0):
     for n in range(1,N):
         for l in range(J): # fill in RHS values for use in tridiag for current iteration
             rhs[l] = (1.j*q)*(psi[l,n-1] + psi[l+2,n-1]) + (1. - (2.*1.j*q) - (1.j*r*V[l]))*psi[l+1,n-1] # deleted factor of r
-            print(rhs[l])
         psi[1:-1,n] = np.dot(Ainv,rhs)
-        psi[:,n] = fBNC(potential, psi[:,n-1])
+        psi[:,n] = fBNC(potential, psi[:,n])
 
 #        for j in range(1,J+1,1): # fill y with CN-solved values
 #            psi[j][n+1] = psi_next[j-1]
