Fixed issues with free particle plotting.  Fixed scaling for harmonic

main/main.py
@@ -18,23 +18,20 @@ def boundary(potential,u,u_last):
     c = (np.sqrt((ut.k+deltak)/deltak) - np.sqrt(ut.k))/deltak
     # For now, all of the boundaries are the same and simple
     if potential == 'free' or potential == 'harmonic':
-        # Periodic boundary condition: let the wave travel through the boundary unchanged
-        # We should stop the simulation when the wave reaches the boundary
-        # This is way more confusing than intended, so leaving for now
-        # https://www.asc.tuwien.ac.at/~arnold/pdf/graz/graz.pdf
-
-        #c = hbar/m # Speed (Griffiths p 65)
-        #u[0] = u[1] + (dx/(c*dt))*(u[1]-u_last[1])
-        #u[-1] = u[-2] - (dx/(c*dt))*(u_last[-2]-u[-2])
-        u[0] = u[1]
+        # Initially we wanted to use open boundary conditions but these require we
+        # know the speed of the wave, which would have been difficult to implement
+        # Instead, these are periodic
+        u[0] = u[-2]
         u[-1] = u[1]
-    elif potential == 'infwell' or potential == 'barrier' or potential == 'hydrogen':
+    elif potential == 'infwell' or potential == 'barrier':
         # Reflective conditions: ghost cell values are simply those of the nearest real cell
         # such that du/dx = 0 at ends
         # http://hplgit.github.io/INF5620/doc/pub/sphinx-wave/._main_wave003.html
         u[0] = u[1]
         u[-1] = u[-2]
-
+    elif potential == 'hydrogen':
+        u[0] = u[-2]
+        u[-1] = u[1]
     return u
 
 # name      type                explanation
@@ -46,10 +43,11 @@ def boundary(potential,u,u_last):
 # fBNC      function pointer    put in array of length J+2, and apply boundary conditions to it
 
 def schrodinger_solve(potential,solver,psi0_name,J,N,xbounds,dt,fBNC):
-    global dx
-    dx = (xbounds[-1]-xbounds[0])/J
+
     x = np.linspace(xbounds[0], xbounds[-1], J) #array of x coordinates
     t = np.arange(0, N, dt)
+    global dx
+    dx = (xbounds[-1]-xbounds[0])/J
 
     psi_0 = np.zeros(J+2) # Initial guess for
     psi_0[1:-1] = ut.fINC(potential,psi0_name, x)
@@ -58,8 +56,8 @@ def schrodinger_solve(potential,solver,psi0_name,J,N,xbounds,dt,fBNC):
     if solver == 'CN':
         psi = cn.cranknicholson(x,t,potential,dt,dx,fBNC,psi_0)
     elif solver == 'CFFT':
-        psi = cf.chebyshev_fft(x,t,potential,fBNC,psi_0, sumcount = 5)
-    return psi, x, t # returned psi is a J by N array of the wavefunction
+        psi = cf.chebyshev_fft(x,t,potential,fBNC,psi_0,sumcount=5)
+    return psi # returned psi is a J by N array of the wavefunction
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
@@ -96,24 +94,34 @@ def main():
     psi0_name    = args.initial
 
     N = 1e3 # Use 1000 time support points
+    if potential == 'free': # This one gets boring after the wave dissipates
+        N = 1e2
 
     xbounds = [0,1] # Say we're looking only at the interval [0,1]
     if potential == 'hydrogen':
         xbounds = [0, 10]
-    elif potential == 'free' or potential == 'harmonic':
+    elif potential == 'free':
         xbounds = [-10, 10]
-    Jorig = J
-    J *= (xbounds[-1] - xbounds[0])
+    elif potential == 'harmonic':
+        xbounds = [-1, 1]
+    Jorig = J # The number of support points in the interval [0,1]
+    J *= (xbounds[-1] - xbounds[0]) # The total number of support points (over entire range)
 
-    psi, x, t = schrodinger_solve(potential,solver,psi0_name,J,N,xbounds,dt,boundary)
-    V = ut.initPotential(potential, x)
+    psi = schrodinger_solve(potential,solver,psi0_name,J,N,xbounds,dt,boundary)
     if (potential == 'hydrogen'):
         psi = psi[0:Jorig,:]
-    elif potential == 'free':
-        psi = psi[J//2-Jorig//2 : J//2 + Jorig//2, :]
+        xbounds = [0,1]
+    elif potential == 'free' or potential == 'harmonic':
+        psi = psi[J//2 - Jorig//2 : J//2 + Jorig//2, :]
+        if potential == 'free':
+            xbounds = [-5,5]
 
-    ut._3DPlot(psi, x, t, V)
-    ut.animPlot(psi, x, t, V)
+    x = np.linspace(xbounds[0], xbounds[-1], Jorig) # x values in interval [0,1]
+    t = np.arange(0, N, dt)
+    V = ut.initPotential(potential, x)
+
+    ut._3DPlot(psi,x,t,xbounds,V)
+    ut.animPlot(psi,x,t,xbounds,V)
 
 # --------------------------------------------------
 main()

main/schrodingerutils.py
@@ -9,7 +9,7 @@ from scipy import constants
 
 m = 1e-31 # approx mass of an e-
 e = 1e-19 # Charge of e-
-a_0 = 0.29177 # Bohr radius
+a_0 = 0.29177 # Bohr radius (not the true value)
 
 e_0 = sp.constants.epsilon_0
 hbar = sp.constants.hbar # use hbar constant value from scipy
@@ -20,12 +20,12 @@ omega = np.sqrt(k/m)
 # Sets up the initial conditions for each potential configuration
 def fINC(potential,psi0_name,x):
     J = len(x)
-    mu = 0.5
+    mu = np.mean(x)
     if potential == 'free':
         if psi0_name == 'wavepacket':
-            width = 1/50 # Variance
+            width = 1/20 # Variance
             a = 1/(width*np.sqrt(2*np.pi))
-            f = a*np.exp(-.5*pow(((x-mu)/width), 2))
+            f = a*np.exp(-.5*pow((x/width), 2))
             return f
     elif potential == 'infwell':
         if psi0_name == "groundstate":
@@ -35,7 +35,7 @@ def fINC(potential,psi0_name,x):
     elif potential == 'finwell':
         if psi0_name == "boundstate":
             L = x[3*J//4] - x[J//4] # Size of the well
-            A = 0.05 # Random guesses at parameters
+            A = 0.05 # Random guesses at parameters to make wavefunction continuous
             B = 0.5
             C = 1
             alpha = k*np.tan(k*L/2)
@@ -47,14 +47,17 @@ def fINC(potential,psi0_name,x):
             return f
     elif potential == 'barrier': # Gaussian on the left side of the barrier
         if psi0_name == "wavepacket":
-            width = 1/50
+            width = 1/35
             a = 1/(width*np.sqrt(2*np.pi))
             mu = x[J//4]
             f = a*np.exp(-.5*pow(((x-mu)/width), 2))
             return f
     elif potential == 'harmonic':
         if psi0_name == "groundstate":
-            f = pow((m*omega/(np.pi*hbar)),.25)*np.exp(-m*omega*pow((x-mu),omega)/(2*hbar))
+            width = 1/20 # Variance
+            a = 1/(width*np.sqrt(2*np.pi))
+            f = a*np.exp(-.5*pow((x/width), 2))
+            plt.show()
             return f
     elif potential == 'hydrogen':
         if psi0_name == 'groundstate':
@@ -75,9 +78,9 @@ def initPotential(name, x): #initialzes a vector corresponding to the potential,
         V[0:J//4] = 1e-34
         V[3*J//4:] = 1e-34
     elif name == 'harmonic':
-        V = 0.5*m*omega*(x-np.mean(x))**2
+        V = 1e-33*x**2
     elif name == 'hydrogen':
-        V[1:] = -pow(e,2)/(4*np.pi*e_0*x[1:])
+        V[1:-1] = -1e-6*pow(e,2)/(4*np.pi*e_0*x[1:-1])
     return V
 
 ### Analytical solutions for the potentials that have them and a getter =============
@@ -117,7 +120,7 @@ def harmonic(x, t):
 
 #====================================================================================
 
-def animPlot(psi,x,t,V,analytical=None): #plotting function that creates time-animated function of Psi vs. x
+def animPlot(psi,x,t,xbounds,V,analytical=None): #plotting function that creates time-animated function of Psi vs. x
     plotnum = 211
     potPlotnum = 212
     if analytical != None:
@@ -140,12 +143,12 @@ def animPlot(psi,x,t,V,analytical=None): #plotting function that creates time-an
         numPlot.set_xlabel('x')
         numPlot.set_ylabel('Numerical $\Psi$')
         numPlot.set_ylim([np.min(psiReal),np.max(psiReal)])
-        numPlot.set_xlim([0, 1])
+        numPlot.set_xlim(xbounds)
         numPlot.legend(loc=1)
 
     numAni = animation.FuncAnimation(fig, animateNumerical, interval=1)
 
-    # If known, show analytical plot
+    # If available, show analytical plot
     if analytical != None:
         truePlot = fig.add_subplot(312)
         psiTrue = analytical(x, t)
@@ -162,7 +165,7 @@ def animPlot(psi,x,t,V,analytical=None): #plotting function that creates time-an
             truePlot.set_xlabel('x')
             truePlot.set_ylabel('Analytical $\Psi$')
             truePlot.set_ylim([np.min(psi),np.max(psi)])
-            truePlot.set_xlim([0, 1])
+            truePlot.set_xlim(xbounds)
             truePlot.legend(loc=1)
 
         trueAni = animation.FuncAnimation(fig, animateTrue, interval=1)
@@ -171,10 +174,10 @@ def animPlot(psi,x,t,V,analytical=None): #plotting function that creates time-an
     pot.plot(x, V)
     pot.set_xlabel('x')
     pot.set_ylabel('V')
-    pot.set_xlim([0, 1])
+    pot.set_xlim(xbounds)
     plt.show()
 
-def _3DPlot(psi,x,t,V,analytical=None): #plotting function that will plot, in 3D, x vs. time vs. psi
+def _3DPlot(psi,x,t,xbounds,V,analytical=None): #plotting function that will plot, in 3D, x vs. time vs. psi
     h = x[1]-x[0] # Distance between support points
     X = len(x) # Number of support points
 
@@ -183,21 +186,23 @@ def _3DPlot(psi,x,t,V,analytical=None): #plotting function that will plot, in 3D
         plotnum = 221
 
     solutions = plt.figure(num=1,figsize=(8,8),dpi=100,facecolor='white')
-    Jmesh, Nmesh = np.meshgrid(t, x)
+    Nmesh, Jmesh = np.meshgrid(t, x)
 
     approxReal = solutions.add_subplot(plotnum,projection='3d')
-    approxReal.plot_surface(Nmesh, Jmesh,psi.real,cmap='rainbow')
+    approxReal.plot_surface(Jmesh,Nmesh,psi.real,cmap='rainbow')
     approxReal.set_zlabel('$\Psi$')
     approxReal.set_ylabel('t')
     approxReal.set_xlabel('x')
     approxReal.set_title('Real component of $\Psi$')
+    approxReal.set_xlim(xbounds[0],xbounds[1])
 
     approxImag = solutions.add_subplot(plotnum+1,projection='3d')
-    approxImag.plot_surface(Nmesh, Jmesh,psi.imag,cmap='rainbow')
+    approxImag.plot_surface(Jmesh,Nmesh,psi.imag,cmap='rainbow')
     approxImag.set_zlabel('$\Psi$')
     approxImag.set_ylabel('t')
     approxImag.set_xlabel('x')
     approxImag.set_title('Imaginary Component of $\Psi$')
+    approxImag.set_xlim(xbounds[0],xbounds[1])
 
     if analytical != None:
         true = fig.add_subplot(122, projection='3d')